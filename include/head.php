e44c8e5b2ac4000f8f42395404e548c5f11ef342
Dristanta Silwal <dristantasilwal003@gmail.com>
2024-09-25 10:10:53 -0700

    html css completed and deployed in heroku

--- include/head.php
+++ include/head.php
@@ -5,5 +5,6 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
+    <script src="index.js"></script>
 
 </head>
---
+++ index.js
@@ -0,0 +1,11 @@
+// comments
+
+/*
+
+multi-line comments
+
+*/
+
+array = [1, 2, 3, 4, 5];
+length = array.length;
+console.log(length);   
