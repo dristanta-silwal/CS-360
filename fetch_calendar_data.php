e095117f074e31dbbe5eb714920ed66e7cd78e3f
Dristanta Silwal <dristantasilwal003@gmail.com>
2024-12-04 18:26:01 +0000

    events added in calendar

--- fetch_calendar_data.php
+++ fetch_calendar_data.php
@@ -1,17 +1,75 @@
 <?php
 session_start();
 
-function fetchCalendarData($showDetails) {
+function fetchCalendarData($showDetails)
+{
     $events = [
         [
-            'title' => $showDetails ? 'Meeting with Team' : 'Busy',
-            'start' => '2024-12-10T10:00:00',
-            'end' => '2024-12-10T11:00:00',
+            'title' => $showDetails ? 'Stat 301' : 'Class',
+            'start' => '2024-12-04T12:30:00',
+            'end' => '2024-12-04T13:20:00',
+            'daysOfWeek' => [1, 3, 5], // Monday, Wednesday, Friday
+            'startTime' => '12:30:00',
+            'endTime' => '13:20:00'
+        ],
+
+        [
+            'title' => $showDetails ? 'CS 210' : 'Class',
+            'start' => '2024-12-05T12:30:00',
+            'end' => '2024-12-05T13:45:00',
+            'daysOfWeek' => [2, 4], // Tuesday, Thursday
+            'startTime' => '12:30:00',
+            'endTime' => '13:45:00'
+        ],
+
+        [
+            'title' => $showDetails ? 'CS 360' : 'Class',
+            'start' => '2024-12-05T14:00:00',
+            'end' => '2024-12-05T15:15:00',
+            'daysOfWeek' => [2, 4], // Tuesday, Thursday
+            'startTime' => '14:00:00',
+            'endTime' => '15:15:00'
         ],
         [
-            'title' => $showDetails ? 'Project Presentation' : 'Busy',
-            'start' => '2024-12-11T14:00:00',
-            'end' => '2024-12-11T15:00:00',
+            'title' => $showDetails ? 'CS 360: Lab' : 'Lab',
+            'start' => '2024-12-05T14:00:00',
+            'end' => '2024-12-05T15:15:00',
+            'daysOfWeek' => [3], // Wednesday
+            'startTime' => '9:30:00',
+            'endTime' => '10:15:00'
+        ],
+
+        [
+            'title' => $showDetails ? 'Guest Services' : 'Work',
+            'start' => '2024-12-09T14:00:00',
+            'end' => '2024-12-09T17:00:00',
+            'daysOfWeek' => [1], // Monday
+            'startTime' => '14:00:00',
+            'endTime' => '17:00:00'
+        ],
+        [
+            'title' => $showDetails ? 'Guest Services' : 'Work',
+            'start' => '2024-12-09T14:00:00',
+            'end' => '2024-12-09T17:00:00',
+            'daysOfWeek' => [2], // Tuesday
+            'startTime' => '15:30:00',
+            'endTime' => '18:00:00'
+        ],
+        [
+            'title' => $showDetails ? 'Guest Services' : 'Work',
+            'start' => '2024-12-09T14:00:00',
+            'end' => '2024-12-09T17:00:00',
+            'daysOfWeek' => [3], // Wednesday
+            'startTime' => '16:00:00',
+            'endTime' => '18:00:00'
+        ],
+        [
+            'title' => $showDetails ? 'OIT TSP' : 'Work',
+            'start' => '2024-12-09T14:00:00',
+            'end' => '2024-12-09T17:00:00',
+            'daysOfWeek' => [1, 4, 5], // Monday, Thursday, Friday
+            'startTime' => '8:00:00',
+            'endTime' => '12:00:00'
         ],
     ];
 
